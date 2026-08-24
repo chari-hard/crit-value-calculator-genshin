8a5c7b1b882278b5c29ca8f84b7fc610918b1fd6
julio <juliocgdlima@gmail.com>
2026-08-24 15:25:03 -0300

    metodos de criação de artefatos

--- src/Artefatos.java
+++ src/Artefatos.java
@@ -1,3 +1,27 @@
 public class Artefatos {
+    private double taxaDoArtefato;
+    private double danoDoArtefato;
 
+    public Artefatos(){
+
+    }
+
+    // get
+
+    public double getTaxa(){
+        return taxaDoArtefato;
+    }
+    public double getDano(){
+        return danoDoArtefato;
+    }
+
+    // set
+
+    public void setTaxa(double taxa){
+        this.taxaDoArtefato = taxa;
+    }
+
+    public void setDano(double dano){
+        this.danoDoArtefato = dano;
+    }
 }
