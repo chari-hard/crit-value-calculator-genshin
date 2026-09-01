7ae81773c274a2ac2a8023b173a85d64212c0a54
julio <juliocgdlima@gmail.com>
2026-09-01 16:28:27 -0300

    Adicionando o método construtor padrão

--- src/Personagem.java
+++ src/Personagem.java
@@ -2,4 +2,8 @@ public class Personagem {
     public double taxaBase;
     public double danoBase;
 
+    public Personagem(double taxa, double dano) {
+        this.taxaBase = taxa;
+        this.danoBase = dano;
+    }
 }
