62d6e72f45d5c41bbbde445a3ebd0231f401628b
julio <juliocgdlima@gmail.com>
2026-09-15 17:00:08 -0300

    Adicionado o método construtor

--- src/Arma.java
+++ src/Arma.java
@@ -1,4 +1,9 @@
 public class Arma {
     public double substatusDano;
     public double substatusTaxa;
+
+    public Arma(double substatusDano, double substatusTaxa){
+        this.substatusDano = substatusDano;
+        this.substatusTaxa = substatusTaxa;
+    }
 }
